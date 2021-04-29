bb21bf989afa72ad5cb721fc71d03ad61e118833
Bastian Woidt <bastianwoidt@gmail.com>
2021-04-29 12:00:15 +0200

    initial commit

--- nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,66 +1,8 @@
 package com.udacity.pricing.domain.price;
 
-import com.udacity.pricing.domain.price.Price;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public class PriceRepository implements CrudRepository<Price, Long> {
+public class PriceRepository {
 
-    @Override
-    public <S extends Price> S save(S s) {
-        return null;
-    }
-
-    @Override
-    public <S extends Price> Iterable<S> saveAll(Iterable<S> iterable) {
-        return null;
-    }
-
-    @Override
-    public Optional<Price> findById(Long aLong) {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public Iterable<Price> findAll() {
-        return null;
-    }
-
-    @Override
-    public Iterable<Price> findAllById(Iterable<Long> iterable) {
-        return null;
-    }
-
-    @Override
-    public long count() {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(Price price) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Price> iterable) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
 }
